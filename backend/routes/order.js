e7c223b7be51bd21c970a874f1655270cb7a9553
praveen-1916 <pkumarbattu@gmail.com>
2024-12-15 11:42:04 +0530

    11th commit

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -248,7 +248,7 @@ orderRouter.get("/get-orders", async (req, res) => {
 
 orderRouter.get("/get-my-orders", async (req, res) => {
   try {
-    const { userId } = req.headers;
+    const userId = req.header("userId");
     if (!userId) {
       res.json({
         success: false,
